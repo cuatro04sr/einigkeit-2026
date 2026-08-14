335ecdbbd900d6bf73fe474631776c10db8371ba
David Montaño Tamayo <cuatro04sr@gmail.com>
2026-08-13 19:14:28 -0500

    fix: pnpm build

--- app/admin/contenido/page.tsx
+++ app/admin/contenido/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState, useMemo, useCallback } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { toast } from "sonner";
 import {
